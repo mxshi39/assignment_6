2c13fbda5ee82fc202bd0eab7bb778ad1f4885a0
mxshi39 <43346879+mxshi39@users.noreply.github.com>
2018-10-26 17:16:40 -0400

    Version 5
    Update comment style

--- cart.js
+++ cart.js
@@ -5,7 +5,7 @@ $(document).ready( function() {
 	console.log(len);
 });
 
-//change img function//
+//change img function
 $(document).on("click","#glaz-drop",function() {
 	var flavor = $("#glaz-drop :selected").text();
 	if (flavor != "Glazing") {
@@ -16,48 +16,48 @@ $(document).on("click","#glaz-drop",function() {
 });
 
 
-// local storage function//
-var addedItem = []; //for temporarily store item if local storage is null//
+// local storage function
+var addedItem = []; //for temporarily store item if local storage is null
 
-// generate ramdom number//
+// generate ramdom number
 function getRandomIndex() {
   return Math.random();
 }
 
-// get all the specific info//
+// get all the specific info
 $(document).on("click",".AddToCart", function() {
 	var img = $(".roll-image img").attr("src");
 	var rollName = $("#roll-name").text();
 	var price = $(".price").text();
 	var quant = $("#quant-drop :selected").text();
 	var glaze = $("#glaz-drop :selected").text();
-	//random id//
+	//random id
 	var num = getRandomIndex();
 	price = price.slice(1,4)*quant;
 	console.log(img);
 	newItem = new Product(img,rollName,price,quant,glaze,num);
-	//get all the items that were already saved//
+	//get all the items that were already saved
 	var savedItem = JSON.parse(localStorage.getItem("addedRolls")); 
-	//depending on whether sth has been saved locally or not, which array will be saved will be different//
+	//depending on whether sth has been saved locally or not, which array will be saved will be different
 	if (savedItem == null) {
-		//add the newly created item to addedItem//
+		//add the newly created item to addedItem
 		addedItem.push(newItem); 
-		//store first item//
+		//store first item
 		localStorage.setItem("addedRolls",JSON.stringify(addedItem)); 
 	}
 	else {
 		savedItem.push(newItem);
-		//store everything again//
+		//store everything again
 		localStorage.setItem("addedRolls",JSON.stringify(savedItem)); 
 	}
-	//update cart count//
+	//update cart count
 	var productArry = JSON.parse(localStorage.getItem("addedRolls"));
 	var len = productArry.length;
 	$("#number").text(len);
 	console.log(len);
 });
 
-//object Product stores all the info got from page after click on add to cart//
+//object Product stores all the info got from page after click on add to cart
 function Product (img,name,price,quant,glaze,num) {
 	this.img = img
 	this.name = name;
--- load.js
+++ load.js
@@ -11,15 +11,15 @@ $(document).ready( function() {
 		var pprice = product.price;
 		var pquant = product.quant;
 		var pid = product.id;
-		//format quant//
+		//format quant
 		pquant = 'x'.concat(pquant);
 		//format price//
 		pprice = '$'.concat(pprice);
-		//insert all info to table//
+		//insert all info to table
 		const template = '<tr class="table-roll"><td class="name"><img class="row-image" src="'+ img + '" />'+pname+'</td><td>'+pglaze+'</td><td class = "quant">'+pquant+'<br/><p localid="'+pid+'" class="delete-item">Delete</p></td><td>'+pprice+'</td></tr>';
 		console.log(template);
 		$(".summary-details tr:last").after(template);
-		// add prices together//
+		// add prices together
 		totalPrice += product.price;
 		console.log(totalPrice);
 	}
@@ -28,13 +28,13 @@ $(document).ready( function() {
 })
 
 
-//remove item//
+//remove item
 $(document).on("click", ".delete-item", function() {
     $(this).parent().parent().remove();
     var pid = $(this).attr('localid');
     console.log(pid);
     var productArry = JSON.parse(localStorage.getItem("addedRolls"));
-    //delete object from array//
+    //delete object from array
     for (i=0; i<productArry.length;i++){
     	var id = productArry[i].id;
     	if (id == pid) {
@@ -42,7 +42,7 @@ $(document).on("click", ".delete-item", function() {
     	}
     }
     console.log(productArry);
-    //update local storage//
+    //update local storage
     localStorage.setItem("addedRolls",JSON.stringify(productArry));
 
  	// update total price//
